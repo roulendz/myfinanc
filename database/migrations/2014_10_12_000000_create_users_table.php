ece8c880717d73955d377ce2bf097461fc5edbf1
roulendz <rolands.zeltins@inbox.lv>
2015-06-06 00:44:56 +0300

    Added Family model and migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
+            $table->integer('familie_id')->nullable();
+            $table->foreign('familie_id')->references('id')->on('families');
 			$table->string('name');
 			$table->string('email')->unique();
 			$table->string('password', 60);
