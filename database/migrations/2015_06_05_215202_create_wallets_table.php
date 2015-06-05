5137e90c14fc80267e31c59f5fa4570b1d733564
roulendz <rolands.zeltins@inbox.lv>
2015-06-06 00:55:33 +0300

    Added Wallet table & Model

---
+++ database/migrations/2015_06_05_215202_create_wallets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWalletsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallets', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('wallet_name');
+            $table->string('wallet_color')->nullable();
+            $table->decimal('wallet_amount', 5, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('wallets');
+    }
+}
